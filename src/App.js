1d4a5e8bd2269320983f1ae83eff5e98415e31e9
Joshua Kelly <joshuakellyeng@gmail.com>
2022-01-15 15:35:23 -0500

    made changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
+	state = {
+		baseURL: 'http://jservice.io/api/random',
+		triviaQuestion: ""
+	}
+
+
+	handleChange = (e) => {
+		this.setState({
+		  [e.target.id]: e.target.value
+		})
+	  }
+
+	  componentDidMount() {
+		console.log('mounted app.js')
+		fetch(this.state.baseURL)
+		  .then(response => response.json())
+		  .then(data => this.setState({ triviaQuestion: data.pop() }))
+		  .catch(error => console.error(error))
+	  }
 	render() {
+		const {question, answer, value} = this.state.triviaQuestion
 		return (
 			<div className='flex'>
 				<h1>Welcome to Jeopardy</h1>
@@ -14,13 +36,14 @@ class App extends Component {
 				</div>
 				<h2>Let's Play!</h2>
 				<button>Get Question</button>
-				<h2>Category: </h2>
-				<h3>Points: </h3>
+				<h2>Category: {this.state.triviaQuestion.category?.title} </h2>
+				<h3>Points: {value} </h3>
 				<div>
-					<h3>Answer: </h3>
+					<h3>Answer: {question} </h3>
 				</div>
 
 				<button>Click To Reveal Question</button>
+				<h1>What is: {answer}</h1>
 			</div>
 		);
 	}
